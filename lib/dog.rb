8dc38053979e46e8a756dad2682c88d53803a99b
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-18 10:21:54 +0300

    completed basics

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+end
+
+class Dog
+    def bark
+      puts "Woof!"
+    end
+  end
+  
+  dog = Dog.new
+  dog.bark
+  
+  class Dog
+    def sit
+      puts "The Dog is sitting"
+    end
+  end
+  
+  dog = Dog.new
+  dog.sit
+  
+  
+  
